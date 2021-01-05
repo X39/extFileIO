88a8cc1754c5061985addea290981e09618c705b
X39 <m.silipo@web.de>
2021-01-06 00:14:26 +0100

    Fixed issues regarding the return code; Cleaned up API; Added Documentation;

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+# Methods
+
+*Note: in the following table, required parameters are marked in **bold***
+
+| Name  | Returns                          |          arg0          |          arg1           |                  arg2                |
+|-------|----------------------------------|------------------------|-------------------------|--------------------------------------|
+| read  | *ARRAY* of *SCALAR*s or *STRING* | **file-path** *STRING* |    read-mode *STRING*   |                                      |
+| write |             *NOTHING*            | **file-path** *STRING* | **write-mode** *STRING* | **contents** **STRING** or **ARRAY** |
+| list  |   *ARRAY* of *file-descriptor*   | **dir-path** *STRING*  |      filter *STRING*    |                                      |
+
+
+*Note: following table describes the different "types" used above. It is assumed that one knows the basic arma types, written in UPPERCASE*
+
+| Type            | Description                                     |
+|-----------------|-------------------------------------------------|
+| file-path       | A path to a valid, existing file.               |
+| dir-path        | A path to a valid, existing directory (folder). |
+| file-descriptor | An array, consisting of two strings. First string always will be either `d` (Directory) or `f` (File). Second string is the path. Example: `["f", "C:/path/to/file.sqf"]` |
+| read-mode       | A combination of the characters
+* `b` (binary - reads the file in, in binary mode, returning an *ARRAY* of *SCALAR*s)
+* `i` (input - automatically set; Tells that you want to read a file)|
+| write-mode      | A combination of the characters
+* `t` (truncate - clear/create file and write)
+* `a` (truncate - clear/create file and write)
+* `o` (input - automatically set; Tells that you want to read a file)
+* `b` (binary - writes the file out, in binary mode, expecting an *ARRAY* of *SCALAR*s as input)|
--- libmain.cpp
+++ libmain.cpp
@@ -16,10 +16,16 @@
 
 #define STR(CONTENT) #CONTENT
 #define ARG_COUNT_MISSMATCH(EXPECTED) "Argument count mismatch! Expected " STR(EXPECTED) "."
+#define ARG_COUNT_MISSMATCH(MIN, MAX) "Argument count mismatch! Expected at least " STR(MIN) " up to " STR(MAX) "."
 #define LONG_RESULT_KEY_NOT_FOUND "Long Result key unknown or expired."
 #define FILE_NOT_FOUND "File not found."
 #define DIRECTORY_NOT_FOUND "Provided directory doesn't exist or is a file."
 #define FAILED_TO_OPEN_FILE_FOR_WRITE "Could not open file for write operation."
+#define PATH_EMPTY_FILE "Path provided is empty."
+#define PATH_EMPTY_DIR "Path provided is empty. Pass in \".\" to make it list the current direcotry."
+#define UNKNOWN_WRITE_MODE "Unknown write mode."
+#define UNKNOWN_READ_MODE "Unknown read mode."
+#define FILE_ARRAY_DATA_PASSED_NOT_ALL_SCALAR "The data inside of the array passed in was not all scalar."
 
 
 using namespace std::string_literals;
@@ -30,6 +36,7 @@ public:
     static constexpr int exec_ok = 0;
     static constexpr int exec_err = -1;
     static constexpr int exec_more = 1;
+    static constexpr const char* msg_unknown_method = "Method passed is not known to extension.";
 private:
     class long_result
     {
@@ -63,7 +70,7 @@ private:
             // set index to end
             index = end;
         }
-        bool is_done() const { return value.length() > index; }
+        bool is_done() const { return value.length() <= index; }
     };
     using func = std::function<sqf::value(std::vector<sqf::value>)>;
 
@@ -88,7 +95,7 @@ private:
         auto it = m_map.find(function);
         if (it == m_map.end())
         {
-            return {};
+            return err(msg_unknown_method);
         }
         return it->second(values);
     }
@@ -180,75 +187,187 @@ DLLEXPORT void STDCALL RVExtension(char* output, int outputSize, const char* fun
 
 DLLEXPORT int STDCALL RVExtensionArgs(char* output, int outputSize, const char* function, const char** argv, int argc)
 {
-    host::instance().execute(output, outputSize, function, argv, argc);
-
-    return 0;
+    auto res = host::instance().execute(output, outputSize, function, argv, argc);
+    return res;
 }
 
 host& host::instance()
 {
     static host h({
-        { "read_file", [](std::vector<sqf::value> values) -> sqf::value {
-            if (values.size() != 1) { return host::err(ARG_COUNT_MISSMATCH(1)); }
+        { "read", [](std::vector<sqf::value> values) -> sqf::value {
+            // Check arg count
+            if (values.size() < 1 || values.size() > 2) { return host::err(ARG_COUNT_MISSMATCH(1, 2)); }
+
+            // Get file path (arg0)
+            if (std::string(values[0]).empty()) { return host::err(PATH_EMPTY_FILE); }
             std::filesystem::path fpath = std::string(values[0]);
+            fpath = std::filesystem::absolute(fpath);
+
+            // ensure file is not directory and exists
             if (!std::filesystem::exists(fpath)) { return host::err(FILE_NOT_FOUND); }
             if (std::filesystem::is_directory(fpath)) { return host::err(FILE_NOT_FOUND); }
-            std::ifstream file(fpath);
+
+            // Get mode (arg1) ("i", "b")
+            std::ios::openmode mode = std::ios::in;
+            if (values.size() >= 1)
+            {
+                auto modes = std::string(values[1]);
+                for (auto c : modes)
+                {
+                    switch (c)
+                    {
+                    case 'i':
+                    case 'I':
+                        mode = mode | std::ios::in;
+                        break;
+                    case 'b':
+                    case 'B':
+                        mode = mode | std::ios::binary;
+                        break;
+                    default:
+                        return host::err(UNKNOWN_READ_MODE);
+                        break;
+                    }
+                }
+            }
+
+            // read in file
+            std::ifstream file(fpath, mode);
             if (!file.is_open() || !file.good()) { return host::err(FILE_NOT_FOUND); }
             std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-            return contents;
+
+            // return array if binary mode was supplied
+            if ((mode & std::ios::binary) == std::ios::binary)
+            {
+                std::vector<sqf::value> out;
+                out.reserve(contents.length());
+                for (auto c : contents)
+                {
+                    out.push_back(sqf::value((float)c));
+                }
+                return out;
+            }
+            else
+            {
+                return contents;
+            }
         } },
-        { "list_dir", [](std::vector<sqf::value> values) -> sqf::value {
-            if (values.size() != 1) { return host::err(ARG_COUNT_MISSMATCH(1)); }
+        { "list", [](std::vector<sqf::value> values) -> sqf::value {
+            if (values.size() < 1 || values.size() > 2) { return host::err(ARG_COUNT_MISSMATCH(1, 2)); }
+
+            if (std::string(values[0]).empty()) { return host::err(PATH_EMPTY_DIR); }
             std::filesystem::path fpath = std::string(values[0]);
+            fpath = fpath.lexically_normal();
+
+
+            std::string ends_with_filter = "";
+            if (values.size() >= 2)
+            {
+                ends_with_filter = std::string(values[1]);
+                std::transform(ends_with_filter.begin(), ends_with_filter.end(), ends_with_filter.begin(),
+                    [](char c) -> char { return (char)std::tolower(c); });
+            }
+
             if (!std::filesystem::exists(fpath)) { return host::err(DIRECTORY_NOT_FOUND); }
             if (!std::filesystem::is_directory(fpath)) { return host::err(DIRECTORY_NOT_FOUND); }
+
             std::vector<sqf::value> paths;
-            
+
             for (auto it = std::filesystem::recursive_directory_iterator(fpath, std::filesystem::directory_options::skip_permission_denied);
                 it != std::filesystem::recursive_directory_iterator();
                 ++it)
             {
+                auto str = std::filesystem::absolute(it->path()).string();
+                auto str_low = str;
+                std::transform(str_low.begin(), str_low.end(), str_low.begin(),
+                    [](char c) -> char { return (char)std::tolower(c); });
+
+                if (str.length() < ends_with_filter.length()) { continue; }
+                if (!ends_with_filter.empty() &&
+                    0 != str_low.compare(
+                    str_low.length() - ends_with_filter.length(),
+                    ends_with_filter.length(),
+                    ends_with_filter)) { continue; }
+
                 paths.push_back({
                     std::filesystem::is_directory(it->path()) ? "d"s : "f"s,
-                    it->path().string()
+                    str
                     });
             }
             return paths;
         } },
-        { "write_truncate", [](std::vector<sqf::value> values) -> sqf::value {
-            if (values.size() != 2) { return host::err(ARG_COUNT_MISSMATCH(2)); }
+        { "write", [](std::vector<sqf::value> values) -> sqf::value {
+            // Check arg count
+            if (values.size() != 3) { return host::err(ARG_COUNT_MISSMATCH(3)); }
+
+            // Get file path (arg0)
+            if (std::string(values[0]).empty()) { return host::err(PATH_EMPTY_FILE); }
             std::filesystem::path fpath = std::string(values[0]);
+            fpath = std::filesystem::absolute(fpath);
+
+            // ensure file is not directory
             if (std::filesystem::is_directory(fpath)) { return host::err(FILE_NOT_FOUND); }
-            auto contents = std::string(values[1]);
 
-            std::ofstream file(fpath, std::ios::trunc | std::ios::out);
+            // Get mode (arg1) ("t", "a", "b", "o")
+            auto modes = std::string(values[1]);
+            std::ios::openmode mode = std::ios::out;
+            for (auto c : modes)
+            {
+                switch (c)
+                {
+                case 'o':
+                case 'O':
+                    mode = mode | std::ios::out;
+                    break;
+                case 't':
+                case 'T':
+                    mode = (mode | std::ios::trunc) & ~std::ios::app;
+                    break;
+                case 'a':
+                case 'A':
+                    mode = (mode | std::ios::app) & ~std::ios::trunc;
+                    break;
+                case 'b':
+                case 'B':
+                    mode = mode | std::ios::binary;
+                    break;
+                default:
+                    return host::err(UNKNOWN_WRITE_MODE);
+                    break;
+                }
+            }
 
+            // open fstream
+            std::ofstream file(fpath, mode);
             if (!file.good())
             {
                 return host::err(FAILED_TO_OPEN_FILE_FOR_WRITE);
             }
 
-            file << contents;
-            file.flush();
-            file.close();
-
-            return {};
-        } },
-        { "write_append", [](std::vector<sqf::value> values) -> sqf::value {
-            if (values.size() != 2) { return host::err(ARG_COUNT_MISSMATCH(2)); }
-            std::filesystem::path fpath = std::string(values[0]);
-            if (std::filesystem::is_directory(fpath)) { return host::err(FILE_NOT_FOUND); }
-            auto contents = std::string(values[1]);
-
-            std::ofstream file(fpath, std::ios::app | std::ios::out);
-
-            if (!file.good())
+            // Get contents to write (arg2)
+            std::string contents;
+            if (values[2].is_array())
             {
-                return host::err(FAILED_TO_OPEN_FILE_FOR_WRITE);
+                auto arr = std::vector<sqf::value>(values[2]);
+                auto out = std::vector<char>();
+                out.reserve(arr.size());
+                for (auto val : arr)
+                {
+                    if (!val.is_scalar())
+                    {
+                        return host::err(FILE_ARRAY_DATA_PASSED_NOT_ALL_SCALAR);
+                    }
+                    out.push_back((char)(int)float(val));
+                }
+                file.write(out.data(), out.size());
+            }
+            else
+            {
+                auto contents = std::string(values[2]);
+
+                file << contents;
             }
 
-            file << contents;
             file.flush();
             file.close();
 
@@ -260,26 +379,78 @@ host& host::instance()
 
 
 
+#if defined(_DEBUG)
+#include <array>
+#include <iostream>
 
-//int main()
-//{
-//    const size_t size = 10000;
-//    char buff[size];
-//
-//    const size_t args1_size = 1;
-//    const char* args1[args1_size] = {
-//        "\"external\""
-//    };
-//    RVExtensionArgs(buff, size, "list_dir", args1, args1_size);
-//
-//    const size_t args2_size = 1;
-//    const char* args2[args2_size] = {
-//        "1"
-//    };
-//    RVExtensionArgs(buff, size, "?", args2, args2_size);
-//    RVExtensionArgs(buff, size, "?", args2, args2_size);
-//    RVExtensionArgs(buff, size, "?", args2, args2_size);
-//    RVExtensionArgs(buff, size, "?", args2, args2_size);
-//    RVExtensionArgs(buff, size, "?", args2, args2_size);
-//    RVExtensionArgs(buff, size, "?", args2, args2_size);
-//}
+size_t strlen(const char* str, size_t max)
+{
+    size_t i;
+    for (i = 0; i < max && str[i] != '\0'; i++) { }
+
+    if (i == max)
+    {
+        std::cerr << "UNTERMINATED C-STYLE STRING DETECTED" << std::endl;
+    }
+    return i;
+}
+template<size_t buff_size, size_t data_size>
+void execute(std::string function, std::array<sqf::value, data_size> data)
+{
+    const char* in_data[data_size];
+    char* in_function = const_cast<char*>(function.c_str());
+    char buff[buff_size];
+    std::array<std::string, data_size> strings;
+
+    std::cout << "Executing '" << function << "' with {";
+
+    for (size_t i = 0; i < data_size; i++)
+    {
+        strings[i] = data[i].to_string();
+        if (i != 0)
+        {
+            std::cout << ", ";
+        }
+        std::cout << " " << strings[i];
+        in_data[i] = strings[i].c_str();
+    }
+    std::cout << " }" << std::endl;
+    auto res = RVExtensionArgs(buff, buff_size, in_function, in_data, data_size);
+    std::cout << "Result: " << res << "\n" <<
+        "Length: " << strlen(buff, buff_size) << "\n";
+    if (res == host::exec_more) { std::cout << buff << std::endl; }
+    else { std::cout << sqf::value::parse(buff).to_string(false) << std::endl; }
+
+    auto orig_res = std::string(buff, strlen(buff, buff_size));
+    const char* in_orig_res[1] = { orig_res.c_str() };
+    while (res == host::exec_more)
+    {
+        res = RVExtensionArgs(buff, buff_size, "?", in_orig_res, 1);
+        std::cout << "Result: " << res << "\n" <<
+                     "Length: " << strlen(buff, buff_size) << "\n" <<
+                     buff << std::endl;
+    }
+    std::cout << std::endl;
+}
+int main()
+{
+    using namespace std::string_literals;
+    using namespace sqf;
+    // Will error and return "more"
+    execute<50>("list_dir"s, std::array{ "\"\""sqf });
+    // Will return array of current working dir
+    execute<1000>("list_dir"s, std::array{ "'.'"sqf, "'.cpp'"sqf });
+    execute<1000>("write"s, std::array{ "'truncate.bin'"sqf, "'tb'"sqf, "[65]"sqf });
+    execute<1000>("write"s, std::array{ "'truncate.bin'"sqf, "'tb'"sqf, "[66]"sqf });
+    execute<1000>("write"s, std::array{ "'truncate.bin'"sqf, "'tb'"sqf, "[67]"sqf });
+    execute<1000>("write"s, std::array{ "'append.bin'"sqf, "'ab'"sqf, "[65]"sqf });
+    execute<1000>("write"s, std::array{ "'append.bin'"sqf, "'ab'"sqf, "[66]"sqf });
+    execute<1000>("write"s, std::array{ "'append.bin'"sqf, "'ab'"sqf, "[67]"sqf });
+    execute<1000>("write"s, std::array{ "'truncate.txt'"sqf, "'t'"sqf, "'A'"sqf });
+    execute<1000>("write"s, std::array{ "'truncate.txt'"sqf, "'t'"sqf, "'B'"sqf });
+    execute<1000>("write"s, std::array{ "'truncate.txt'"sqf, "'t'"sqf, "'C'"sqf });
+    execute<1000>("write"s, std::array{ "'append.txt'"sqf, "'a'"sqf, "'A'"sqf });
+    execute<1000>("write"s, std::array{ "'append.txt'"sqf, "'a'"sqf, "'B'"sqf });
+    execute<1000>("write"s, std::array{ "'append.txt'"sqf, "'a'"sqf, "'C'"sqf });
+}
+#endif
